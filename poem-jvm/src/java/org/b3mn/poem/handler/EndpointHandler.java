3e331f61141dc1bbe66954d906a62c7d4cce0b47
Sandile Keswa <sandile.keswa@gmail.com>
2013-06-06 14:21:10 -0400

    fixed the service based addons

--- poem-jvm/src/java/org/b3mn/poem/handler/EndpointHandler.java
+++ poem-jvm/src/java/org/b3mn/poem/handler/EndpointHandler.java
@@ -1,8 +1,8 @@
 package org.b3mn.poem.handler;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Properties;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,45 +10,97 @@ import javax.servlet.http.HttpServletResponse;
 import org.b3mn.poem.Identity;
 import org.b3mn.poem.util.HandlerWithoutModelContext;
 
+import com.conx.bi.app.reporting.dao.services.Endpoint;
+import com.conx.bi.app.reporting.dao.services.IReportingDAOServicePortType;
+
 @HandlerWithoutModelContext(uri = "/endpoint")
 public class EndpointHandler extends HandlerBase {
-	Properties props = null;
-	final static String configPreFix = "profile.stencilset.mapping.";
-	final static String defaultSS = "/stencilsets/bpmn/bpmn.json";
+	private static final boolean isTesting = true;
 
-//	private IReportingDAOServicePortType reportingDAOService;
+	private IReportingDAOServicePortType reportingDAOService;
 
 	@Override
 	public void init() {
-		// Load properties
-		FileInputStream in;
+		// JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
+		// factory.getInInterceptors().add(new LoggingInInterceptor());
+		// factory.getOutInterceptors().add(new LoggingOutInterceptor());
+		// factory.setServiceClass(IReportingDAOService.class);
+		// factory.setAddress("http://71.162.140.200:8181/cxf/com/conx/bi/app/reporting/dao/services/IReportingDAOService");
+		// this.reportingDAOService = ((IReportingDAOService)
+		// factory.create()).getIReportingDAOServicePort();
+	}
 
-		// initialize properties from backend.properties
-		try {
-//			JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
-//			factory.getInInterceptors().add(new LoggingInInterceptor());
-//			factory.getOutInterceptors().add(new LoggingOutInterceptor());
-//			factory.setServiceClass(IReportingDAOService.class);
-//			factory.setAddress("http://71.162.140.200:8181/cxf/com/conx/bi/app/reporting/dao/services/IReportingDAOService");
-//			this.reportingDAOService = ((IReportingDAOService) factory.create()).getIReportingDAOServicePort();
-			
-			in = new FileInputStream(this.getBackendRootDirectory() + "/WEB-INF/backend.properties");
-			props = new Properties();
-			props.load(in);
-			in.close();
-		} catch (Exception e) {
-			props = new Properties();
+	private List<Endpoint> getEndpoints() {
+		if (!isTesting) {
+			return this.reportingDAOService.getAllEndpoints();
+		} else {
+			List<Endpoint> list = new ArrayList<Endpoint>();
+			Endpoint temp = new Endpoint();
+			temp.setId(101L);
+			temp.setCode("endpoint-mysql");
+			temp.setName("MySQL Endpoint");
+			list.add(temp);
+			temp = new Endpoint();
+			temp.setId(102L);
+			temp.setCode("endpoint-oracle");
+			temp.setName("Oracle Endpoint");
+			list.add(temp);
+			temp = new Endpoint();
+			temp.setId(103L);
+			temp.setCode("endpoint-mongo");
+			temp.setName("MongoDB Endpoint");
+			list.add(temp);
+			return list;
 		}
 	}
 
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response, Identity subject, Identity object) throws IOException {
 		if ((request.getParameter("userId") != null) && (request.getParameter("call") != null)) {
-//			String userId = request.getParameter("userId");
+			// String userId = request.getParameter("userId");
 			String call = request.getParameter("call");
 			if ("getEndpointsByTenant".equals(call)) {
+				StringBuffer buffer = new StringBuffer();
+				buffer.append('[');
+				List<Endpoint> endpoints = getEndpoints();
+				boolean isFirst = true;
+				for (Endpoint endpoint : endpoints) {
+					if (!isFirst)
+						buffer.append(',');
+					
+					buffer.append('{');
+
+					buffer.append("\"title\":\"");
+					if (endpoint.getName() == null) {
+						buffer.append("<undefined>");
+					} else {
+						buffer.append(endpoint.getName());
+					}
+					buffer.append("\",");
+
+					buffer.append("\"value\":\"");
+					if (endpoint.getCode() == null) {
+						buffer.append(0);
+					} else {
+						buffer.append(endpoint.getCode());
+					}
+					buffer.append("\",");
+
+					buffer.append("\"id\":");
+					if (endpoint.getId() == null) {
+						buffer.append(0);
+					} else {
+						buffer.append(endpoint.getId());
+					}
+
+					buffer.append('}');
+
+					if (isFirst)
+						isFirst = false;
+				}
+				buffer.append(']');
 				response.setContentType("application/xml");
-				response.getWriter().write("[{\"title\":\"MySQL*\",\"value\":1,\"id\":1},{\"title\":\"PostgreSQL*\",\"value\":2,\"id\":2},{\"title\":\"Oracle*\",\"value\":3,\"id\":3}]");
+				response.getWriter().write(buffer.toString());
 				response.setStatus(200);
 			}
 		} else {
--- poem-jvm/src/java/org/b3mn/poem/handler/ReportingHandler.java
+++ poem-jvm/src/java/org/b3mn/poem/handler/ReportingHandler.java
@@ -1,8 +1,8 @@
 package org.b3mn.poem.handler;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Properties;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,43 +10,85 @@ import javax.servlet.http.HttpServletResponse;
 import org.b3mn.poem.Identity;
 import org.b3mn.poem.util.HandlerWithoutModelContext;
 
+import com.conx.bi.app.reporting.dao.services.IReportingDAOServicePortType;
+import com.conx.bi.app.reporting.dao.services.ReportTemplate;
+
 @HandlerWithoutModelContext(uri = "/reporting")
 public class ReportingHandler extends HandlerBase {
-	Properties props = null;
-
-//	private IReportingDAOServicePortType reportingDAOService;
+	private static final boolean isTesting = true;
+	
+	private IReportingDAOServicePortType reportingDAOService;
 
 	@Override
 	public void init() {
-		// Load properties
-		FileInputStream in;
+//		JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
+//		factory.getInInterceptors().add(new LoggingInInterceptor());
+//		factory.getOutInterceptors().add(new LoggingOutInterceptor());
+//		factory.setServiceClass(IReportingDAOService.class);
+//		factory.setAddress("http://71.162.140.200:8181/cxf/com/conx/bi/app/reporting/dao/services/IReportingDAOService");
+//		this.reportingDAOService = ((IReportingDAOService) factory.create()).getIReportingDAOServicePort();
+	}
 
-		// initialize properties from backend.properties
-		try {
-//			JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
-//			factory.getInInterceptors().add(new LoggingInInterceptor());
-//			factory.getOutInterceptors().add(new LoggingOutInterceptor());
-//			factory.setServiceClass(IReportingDAOService.class);
-//			factory.setAddress("http://71.162.140.200:8181/cxf/com/conx/bi/app/reporting/dao/services/IReportingDAOService");
-//			this.reportingDAOService = ((IReportingDAOService) factory.create()).getIReportingDAOServicePort();
-
-			in = new FileInputStream(this.getBackendRootDirectory() + "/WEB-INF/backend.properties");
-			props = new Properties();
-			props.load(in);
-			in.close();
-		} catch (Exception e) {
-			props = new Properties();
+	private List<ReportTemplate> getReportingTemplates() {
+		if (!isTesting) {
+			return this.reportingDAOService.getAllReportTemplates();
+		} else {
+			List<ReportTemplate> list = new ArrayList<ReportTemplate>();
+			ReportTemplate temp = new ReportTemplate();
+			temp.setId(1001L);
+			temp.setName("Default Template");
+			list.add(temp);
+			temp = new ReportTemplate();
+			temp.setId(1002L);
+			temp.setName("Basic Template");
+			list.add(temp);
+			temp = new ReportTemplate();
+			temp.setId(1003L);
+			temp.setName("Advanced Template");
+			list.add(temp);
+			return list;
 		}
 	}
 
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response, Identity subject, Identity object) throws IOException {
 		if ((request.getParameter("userId") != null) && (request.getParameter("call") != null)) {
-//			String userId = request.getParameter("userId");
+			// String userId = request.getParameter("userId");
 			String call = request.getParameter("call");
 			if ("getReportTemplatesByTenant".equals(call)) {
+				StringBuffer buffer = new StringBuffer();
+				buffer.append('[');
+				List<ReportTemplate> templates = getReportingTemplates();
+				boolean isFirst = true;
+				for (ReportTemplate template : templates) {
+					if (!isFirst)
+						buffer.append(',');
+					
+					buffer.append('{');
+
+					buffer.append("\"name\":\"");
+					if (template.getName() == null) {
+						buffer.append("<undefined>");
+					} else {
+						buffer.append(template.getName());
+					}
+					buffer.append("\",");
+
+					buffer.append("\"value\":");
+					if (template.getId() == null) {
+						buffer.append(0);
+					} else {
+						buffer.append(template.getId());
+					}
+
+					buffer.append('}');
+					
+					if (isFirst)
+						isFirst = false;
+				}
+				buffer.append(']');
 				response.setContentType("application/xml");
-				response.getWriter().write("[{\"name\":\"Basic Job\",\"value\":1},{\"name\":\"Quick Job\",\"value\":2},{\"name\":\"Complex Job\",\"value\":3}]");
+				response.getWriter().write(buffer.toString());
 				response.setStatus(200);
 			}
 		} else {
